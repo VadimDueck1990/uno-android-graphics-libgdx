cde9ecc534ab681c85839b81e47f5e72cad970d7
vdueck <vdueck@stud.hs-heilbronn.de>
2020-01-03 16:35:10 +0100

    kunktionierender UI-Stand

--- libgdx_tutorial/core/src/de/hhn/aib/swlab/wise1920/group06/exercise3/MyUnoGame.java
+++ libgdx_tutorial/core/src/de/hhn/aib/swlab/wise1920/group06/exercise3/MyUnoGame.java
@@ -14,11 +14,35 @@ import java.util.List;
 
 
 public class MyUnoGame extends ApplicationAdapter {
+	// dummy numbers to test hand generation
+	int ownNumber;
+	int leftNumber;
+	int topNumber;
+	int rightNumber;
+
+	private float discardAngle; // rotation angle of the discard cards
+
+	static final float WIDTH_HEIGHT_RATIO = 1.44845f; // the width to height ratio
+	static final float WIDTH_SCREEN_RATIO = 7.66667f; //  the card to screen ratio
+	static final float OTHERS_SCREEN_RATIO = 11.0f; // the card to screen ratio of the other hands
+	static final float CARD_ROTATION = -7.0f;
+
 	private Stage stage;
 	private List<Image> ownHand;
 	private List<Image> leftHand;
 	private List<Image> topHand;
 	private List<Image> rightHand;
+	private List<Image> deck;
+	private List<Image> discardStack;
+
+	// own and others card size
+	float ownCardWidth; // actual card width
+	float ownCardHeight;
+	float foreignCardWidth; // actual card width
+	float foreignCardHeight;
+	// positions of deck and discardstack
+	Vector2 deckPosition;
+	Vector2 discardStackPosition;
 
 	
 	@Override
@@ -26,28 +50,31 @@ public class MyUnoGame extends ApplicationAdapter {
 		// create a stage and set it to full screen
 		stage = new Stage(new ScreenViewport());
 
-		// dummy numbers to test hand generation
-		int ownNumber = 9;
-		int leftNumber = 5;
-		int topNumber = 7;
-		int rightNumber = 5;
+		// initialize dummy numbers to test hand generation
+		ownNumber = 7;
+		leftNumber = 7;
+		topNumber = 7;
+		rightNumber = 7;
+
+		discardAngle = 0.0f;
 
 		ownHand  = new ArrayList<>();
 		leftHand = new ArrayList<>();
 		topHand = new ArrayList<>();
 		rightHand = new ArrayList<>();
 
-		final float WIDTH_HEIGHT_RATIO = 1.44845f; // the width to height ratio
-		final float WIDTH_SCREEN_RATIO = 7.66667f; //  the card to screen ratio
-		final float OTHERS_SCREEN_RATIO = 11.0f; // the card to screen ratio of the other hands
-		final float CARD_ROTATION = -7.0f;
-
-		// own card size
-		final float ownCardWidth = Gdx.graphics.getWidth() / WIDTH_SCREEN_RATIO; // actual card width
-		final float ownCardHeight = ownCardWidth * WIDTH_HEIGHT_RATIO;
-		// all other card sizes
-		final float foreignCardWidth = Gdx.graphics.getWidth() / OTHERS_SCREEN_RATIO; // actual card width
-		final float foreignCardHeight = foreignCardWidth * WIDTH_HEIGHT_RATIO;
+		// own and others card size
+		ownCardWidth = Gdx.graphics.getWidth() / WIDTH_SCREEN_RATIO; // actual card width
+		ownCardHeight = ownCardWidth * WIDTH_HEIGHT_RATIO;
+		foreignCardWidth = Gdx.graphics.getWidth() / OTHERS_SCREEN_RATIO; // actual card width
+		foreignCardHeight = foreignCardWidth * WIDTH_HEIGHT_RATIO;
+		// positions of deck and discardstack
+		deckPosition = new Vector2();
+		discardStackPosition = new Vector2();
+		deckPosition.x = Gdx.graphics.getWidth() / 3 - ownCardWidth / 2.0f;
+		deckPosition.y = Gdx.graphics.getHeight() / 2 - ownCardHeight / 2.0f;
+		discardStackPosition.x = Gdx.graphics.getWidth()/3.0f*2.0f - ownCardWidth / 2.0f;
+		discardStackPosition.y = Gdx.graphics.getHeight() / 2 - ownCardHeight / 2.0f;
 
 		// fill all hands with dummy cards
 		createOwnHand(ownNumber, ownCardWidth, ownCardHeight);
@@ -70,9 +97,25 @@ public class MyUnoGame extends ApplicationAdapter {
 		otherCard.setPosition(Gdx.graphics.getWidth() - ownCardWidth*2, Gdx.graphics.getHeight()-foreignCardHeight/3 * 2);
 		stage.addActor(otherCard);*/
 
-		placeOwnCards(Gdx.graphics.getWidth(), ownCardWidth, ownCardHeight, CARD_ROTATION);
-		placeTopCards(Gdx.graphics.getWidth(), foreignCardWidth, foreignCardHeight, CARD_ROTATION, topHand);
-		placeLeftCards(Gdx.graphics.getHeight(), foreignCardWidth, foreignCardHeight, CARD_ROTATION, leftHand);
+		// add Deck
+		Texture deckTexture = new Texture(Gdx.files.internal("Deck.png"));
+		Image deck = new Image(deckTexture);
+		deck.setSize(ownCardWidth, ownCardHeight);
+		deck.setOrigin(ownCardWidth/2, ownCardHeight/2);
+		deck.setPosition(deckPosition.x, deckPosition.y);
+		stage.addActor(deck);
+
+		addCardToDiscardStack("Blue_0.png");
+		addCardToDiscardStack("Green_5.png");
+		addCardToDiscardStack("Red_5.png");
+		//addCardToDiscardStack("Blue_Draw.png");
+		//addCardToDiscardStack("Yellow_Draw.png");
+		// addCardToDiscardStack("Blue_Skip.png");
+
+		placeOwnCards(Gdx.graphics.getWidth());
+		placeTopCards(Gdx.graphics.getWidth(), topHand);
+		placeLeftCards(Gdx.graphics.getHeight(), leftHand);
+		placeRightCards(Gdx.graphics.getHeight(), rightHand);
 
 		// same for card
 		/*Texture wildTexture = new Texture(Gdx.files.internal("Wild_Draw.png"));
@@ -119,13 +162,13 @@ public class MyUnoGame extends ApplicationAdapter {
 	}
 
 	// place the hand of the own player
-	public void placeOwnCards(float screenWidth, float cardWidth, float cardHeight, float rot) {
+	public void placeOwnCards(float screenWidth) {
 	    // define the constants
-        final float rotation = rot;   // rotation in degrees
-        final float rotationOffset = MathUtils.sinDeg(rotation) * cardHeight;     // offset to show the edge of the card on the left
-		float offset = -2.0f/3.0f * cardWidth;
+        final float rotation = CARD_ROTATION;   // rotation in degrees
+        final float rotationOffset = MathUtils.sinDeg(rotation) * ownCardHeight;     // offset to show the edge of the card on the left
+		float offset = -2.0f/3.0f * ownCardWidth;
 		if(ownHand.size() > 8) {
-		    offset = (screenWidth - cardWidth *3.0f) / ((float)ownHand.size() - 1.0f);
+		    offset = (screenWidth - ownCardWidth *3.0f) / ((float)ownHand.size() - 1.0f);
         }
 		final float offsetGlobal = (((float)ownHand.size() * offset + offset) / 2.0f) - rotationOffset;
         float xPosCard;
@@ -135,22 +178,22 @@ public class MyUnoGame extends ApplicationAdapter {
         // calculate the x-pos of every card
 		for(int i = ownHand.size(); i > 0; i--) {
 			float index = (float)i;
-			xPosCard = (screenWidth / 2.0f) + ((offset * index - cardWidth / 2.0f)) - offsetGlobal;
+			xPosCard = (screenWidth / 2.0f) + ((offset * index - ownCardWidth / 2.0f)) - offsetGlobal;
 
-			ownHand.get(ownHand.size()-i).setPosition(xPosCard, -cardHeight/3.0f);
+			ownHand.get(ownHand.size()-i).setPosition(xPosCard, -ownCardHeight/3.0f);
             ownHand.get(i-1).rotateBy(rotation);
 			stage.addActor(ownHand.get(i - 1));
 		}
 	}
 
 	// place the hand of the own player
-	public void placeTopCards(float screenWidth, float cardWidth, float cardHeight, float rot, List<Image> hand) {
+	public void placeTopCards(float screenWidth, List<Image> hand) {
 		// define the constants
-		final float rotation = rot + 180.0f;   // rotation in degrees
-		final float rotationOffset = MathUtils.sinDeg(rotation) * cardHeight;     // offset to show the edge of the card on the left
-		float offset = -2.0f/3.0f * cardWidth;
+		final float rotation = CARD_ROTATION + 180.0f;   // rotation in degrees
+		final float rotationOffset = MathUtils.sinDeg(rotation) * foreignCardHeight;     // offset to show the edge of the card on the left
+		float offset = -2.0f/3.0f * foreignCardWidth;
 		if(hand.size() > 8) {
-			offset = (screenWidth - cardWidth *6.0f) / ((float)hand.size() - 1.0f);
+			offset = (screenWidth - foreignCardWidth *6.0f) / ((float)hand.size() - 1.0f);
 		}
 		final float offsetGlobal = (((float)hand.size() * offset + offset) / 2.0f) - rotationOffset;
 		float xPosCard;
@@ -160,24 +203,24 @@ public class MyUnoGame extends ApplicationAdapter {
 		// calculate the x-pos of every card
 		for(int i = hand.size(); i > 0; i--) {
 			float index = (float)i;
-			xPosCard = (screenWidth / 2.0f) + ((offset * index + cardWidth / 2.0f)) - offsetGlobal;
+			xPosCard = (screenWidth / 2.0f) + ((offset * index + foreignCardWidth / 2.0f)) - offsetGlobal;
 
-			hand.get(hand.size()-i).setPosition(xPosCard, Gdx.graphics.getHeight()+ cardHeight * 1.0f/3.0f);
+			hand.get(hand.size()-i).setPosition(xPosCard, Gdx.graphics.getHeight()+ foreignCardHeight * 1.0f/3.0f);
 			hand.get(i-1).rotateBy(rotation);
 			stage.addActor(hand.get(i - 1));
 		}
 	}
 
 	// place the hand of the own player
-	public void placeLeftCards(float screenHeight, float cardWidth, float cardHeight, float rot, List<Image> hand) {
+	public void placeLeftCards(float screenHeight, List<Image> hand) {
 		// define the constants
-		final float rotation = rot + 270.0f;   // rotation in degrees
-		final float rotationOffset = MathUtils.sinDeg(rotation-270.0f) * cardHeight;     // offset to show the edge of the card on the left
-		float offset = -2.0f/3.0f * cardWidth;
+		final float rotation = CARD_ROTATION + 270.0f;   // rotation in degrees
+		final float rotationOffset = MathUtils.sinDeg(rotation-270.0f) * foreignCardHeight;     // offset to show the edge of the card on the left
+		float offset = -2.0f/3.0f * foreignCardWidth;
 		if(hand.size() > 8) {
-			offset = (screenHeight - cardWidth *2.0f) / ((float)hand.size() - 1.0f);
+			offset = (screenHeight - foreignCardWidth *2.0f) / ((float)hand.size() - 1.0f);
 		}
-		final float offsetGlobal = (((float)hand.size() * offset + offset) / 2.0f) + rotationOffset - cardWidth;
+		final float offsetGlobal = (((float)hand.size() * offset + offset) / 2.0f) + rotationOffset - foreignCardWidth;
 		float yPosCard;
 
 		Gdx.app.log("Handwidth: ", String.valueOf(screenHeight));
@@ -185,11 +228,47 @@ public class MyUnoGame extends ApplicationAdapter {
 		// calculate the x-pos of every card
 		for(int i = hand.size(); i > 0; i--) {
 			float index = (float)i;
-			yPosCard = (screenHeight / 2.0f) + ((offset * index - cardWidth / 2.0f)) - offsetGlobal;
+			yPosCard = (screenHeight / 2.0f) + ((offset * index - foreignCardWidth / 2.0f)) - offsetGlobal;
 
-			hand.get(hand.size()-i).setPosition( -cardHeight/3.0f, yPosCard);
+			hand.get(hand.size()-i).setPosition( -foreignCardHeight/3.0f, yPosCard);
 			hand.get(i-1).rotateBy(rotation);
 			stage.addActor(hand.get(i - 1));
 		}
+	}
+
+	// place the hand of the own player
+	public void placeRightCards(float screenHeight, List<Image> hand) {
+		// define the constants
+		final float rotation = CARD_ROTATION + 90.0f;   // rotation in degrees
+		final float rotationOffset = MathUtils.sinDeg(rotation-90.0f) * foreignCardHeight;     // offset to show the edge of the card on the left
+		float offset = -2.0f/3.0f * foreignCardWidth;
+		if(hand.size() > 8) {
+			offset = (screenHeight - foreignCardWidth *2.0f) / ((float)hand.size() - 1.0f);
+		}
+		final float offsetGlobal = (((float)hand.size() * offset + offset) / 2.0f);
+		float yPosCard;
+
+		Gdx.app.log("Handwidth: ", String.valueOf(screenHeight));
+		Gdx.app.log("Handwidth: ", String.valueOf(offsetGlobal * 2.0f));
+		// calculate the x-pos of every card
+		for(int i = hand.size(); i > 0; i--) {
+			float index = (float)i;
+			yPosCard = (screenHeight / 2.0f) + ((offset * index - foreignCardWidth / 2.0f)) - offsetGlobal;
+
+			hand.get(hand.size()-i).setPosition( Gdx.graphics.getWidth()+foreignCardHeight/3.0f, yPosCard);
+			hand.get(i-1).rotateBy(rotation);
+			stage.addActor(hand.get(i - 1));
+		}
+	}
+
+	public void addCardToDiscardStack(String imagePath) {
+		Texture discardTexture = new Texture(Gdx.files.internal(imagePath));
+		Image discard = new Image(discardTexture);
+		discard.setSize(ownCardWidth, ownCardHeight);
+		discard.setOrigin(ownCardWidth/2, ownCardHeight/2);
+		discard.setPosition(discardStackPosition.x, discardStackPosition.y);
+		discard.rotateBy(discardAngle);
+		discardAngle -= 40.0f;
+		stage.addActor(discard);
 	}
 }
